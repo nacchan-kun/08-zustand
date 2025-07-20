bb1a0752a88e781cba6b9e5cc5fad2ea9cc566a5
nacchan-kun <sendono.nc17@gmail.com>
2025-07-20 15:34:28 +0300

    vc

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: false,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
 };
 
 export default nextConfig;
