756f8c68fe15acc64806b384594928231c54054f
nacchan-kun <sendono.nc17@gmail.com>
2025-07-21 19:18:01 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
 };
 
 export default nextConfig;
